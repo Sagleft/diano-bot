c366c2b0b107f5b23c80e0ea0c76e7a7a03e10b4
Sagleft <sagleft@inbox.ru>
2020-09-24 14:05:28 +0400

    added functionality for checking posts in the database

--- src/Messengers/MessengerBase.php
+++ src/Messengers/MessengerBase.php
@@ -2,8 +2,11 @@
 	namespace App\Messengers;
 
 	class MessengerBase {
+		public $tag  = 'default';
 		public $name = 'Default messenger';
 		public $last_error = '';
+		
+		protected $db = null; //DataBase object
 		
 		public function __construct($connection_data = []) {
 			$this->connect($connection_data);
@@ -12,6 +15,10 @@
 		public function connect($connection_data = []): bool {
 			//placeholder
 			return true;
+		}
+		
+		public function setDB($dbref = null) {
+			$this->db = &$dbref;
 		}
 
 		public function getChannelContent($channel_ID = ''): array {
@@ -34,5 +41,23 @@
 		public function importMessages($channelid = '', $messages_arr = []): bool {
 			//placeholder
 			return true;
+		}
+		
+		public function markPostUsed($post_id = '0', $channelid = ''): bool {
+			$sql_query  = "INSERT INTO channels SET ";
+			$sql_query .= "last_post_id='" . $post_id . "',";
+			$sql_query .= "channelid='" . $channelid . "',";
+			$sql_query .= "messenger='" . $this->tag . "'";
+
+			return $this->db->tryQuery($sql_query);
+		}
+		
+		public function checkPostISUsed($post_id = '0', $channelid = ''): bool {
+			$sql_query  = "SELECT id FROM channels where messenger='" . $this->tag . "'";
+			$sql_query .= " AND channelid='" . $channelid . "'";
+			$sql_query .= " AND last_post_id='" . $post_id . "'";
+			$sql_query .= " LIMIT 1";
+
+			return $this->db->checkRowExists($sql_query);
 		}
 	}
