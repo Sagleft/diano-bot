f8365540b68392bda2774a1a9cbacb71315c8720
Sagleft <sagleft@inbox.ru>
2020-10-20 19:45:30 +0400

    Update Message.php

--- src/Messengers/Content/Message.php
+++ src/Messengers/Content/Message.php
@@ -4,8 +4,12 @@
 	class Message {
 		public $id = 1;
 		public $text = '';
+		public $type = 'text'; //text, document
 		public $image_url = '';
-		
+
+		public $document_path = '';
+		public $document_name = '';
+
 		public function __construct($newtext = '') {
 			$this->text = $newtext;
 		}
